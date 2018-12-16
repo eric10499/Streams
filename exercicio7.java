b16a105bfa7e00675ba7fd43bd24daee509fc707
eric10499 <34514368+eric10499@users.noreply.github.com>
2018-12-16 20:11:01 +0100

    Update exercicio7.java

--- exercicio7.java
+++ exercicio7.java
@@ -1,3 +1,29 @@
+/*
+ * Streams3_2.java
+ * 
+ * Copyright 2018 ERIC <ERIC@DESKTOP-5T1N085>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * Escribe un programa que, utilizando la clase BufferedWriter, escriba 10 filas de caracteres en un fichero de texto y después de escribir cada fila salta una línea con el método newLine()
+ * 
+ * Fet per Eric Visier Sánchez
+ * 
+ */
+
 package m9;
 
 import java.io.BufferedWriter;
